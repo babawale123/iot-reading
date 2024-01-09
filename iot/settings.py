a192f24708830924c3dc01338fbac55a56523f0e
babawale123 <waleramson123@gmail.com>
2024-01-09 19:30:08 +0000

    iot

--- iot/settings.py
+++ iot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#35*+lmf^3o+yy&r(63wybku)=ggsl2e7&!42a90jm*ui1a#-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Ramson.pythonanywhere.com"]
 
 
 # Application definition
